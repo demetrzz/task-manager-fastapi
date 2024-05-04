3945a4c352bcfb50fca1e7d1ace51995e469ac87
demetrzz <paha.gtn@gmail.com>
2024-05-05 00:46:34 +0300

    minor router changes

--- src/task_manager/api/users_router.py
+++ src/task_manager/api/users_router.py
@@ -8,28 +8,24 @@ users_router = APIRouter()
 
 
 users_router.include_router(
-    fastapi_users.get_auth_router(auth_backend), prefix="/auth/jwt", tags=["auth"]
+    fastapi_users.get_auth_router(auth_backend), prefix="/auth/jwt",
 )
 users_router.include_router(
     fastapi_users.get_register_router(UserRead, UserCreate),
     prefix="/auth",
-    tags=["auth"],
 )
-# users_router.include_router(
-#     fastapi_users.get_reset_password_router(),
-#     prefix="/auth",
-#     tags=["auth"],
-# )
-# users_router.include_router(
-#     fastapi_users.get_verify_router(UserRead),
-#     prefix="/auth",
-#     tags=["auth"],
-# )
-# users_router.include_router(
-#     fastapi_users.get_users_router(UserRead, UserUpdate),
-#     prefix="/users",
-#     tags=["users"],
-# )
+users_router.include_router(
+    fastapi_users.get_reset_password_router(),
+    prefix="/auth",
+)
+users_router.include_router(
+    fastapi_users.get_verify_router(UserRead),
+    prefix="/auth",
+)
+users_router.include_router(
+    fastapi_users.get_users_router(UserRead, UserUpdate),
+    prefix="/users",
+)
 
 
 @users_router.get("/authenticated-route")
